fd3a6ac222c073297c15d9d9aba4f89c2093877e
Saswata Dutta <saswat.dutta@gmail.com>
2022-04-30 21:03:41 +0530

    Parser combinator (#1)
    * add initial parsing for terminals
    
    * add single valued obj and arr
    
    * todo bug jarr and job null in jval choice : somehow resolve lazy defn
    
    * wip make varargs to break init cycle in SEQ(parsers) call
    
    * make it work with repeat in jarr and jobj
    
    * makes json parser combinator final

---
+++ src/main/scala/org/saswata/JsonParserCombinator.scala
@@ -0,0 +1,238 @@
+package org.saswata
+
+import scala.util.Try
+
+object JsonParserCombinator {
+  // https://www.json.org/json-en.html
+
+  def skipWs(input: String, index: Int): Int = {
+    var pos = index
+    while (pos < input.length && input(pos).isWhitespace) {
+      pos += 1
+    }
+    pos
+  }
+
+  def skipTillQuote(input: String, index: Int): Option[Int] = {
+    var pos = index
+    while (pos < input.length && input(pos) != '"') {
+      pos += 1
+    }
+
+    if (pos == input.length) Option.empty else Some(pos)
+  }
+
+  val DELIMS: Set[Char] = ",}]".toSet
+
+  def skipNumber(input: String, index: Int): Option[Int] = {
+    var pos = skipWs(input, index)
+    while (pos < input.length && !(input(pos).isWhitespace || DELIMS.contains(input(pos)))) {
+      pos += 1
+    }
+
+    val n = Try(input.slice(index, pos).toDouble)
+    if (n.isSuccess) Some(pos) else Option.empty
+  }
+
+  def matchAt(target: String, input: String, index: Int): Option[Int] = {
+    var pos = skipWs(input, index)
+    var i = 0
+    while (pos < input.length && i < target.length && input(pos) == target(i)) {
+      i += 1
+      pos += 1
+    }
+
+    if (i == target.length) Some(pos)
+    else Option.empty
+  }
+
+  case class Match(begin: Int, end: Int)
+
+  case class Mismatch(expected: String, begin: Int)
+
+  case class ParserState(input: String, index: Int = 0,
+                         pending: Vector[Match] = Vector.empty[Match],
+                         mismatches: Set[Mismatch] = Set.empty[Mismatch],
+                         stack: Vector[JValue] = Vector.empty[JValue]) {
+
+    def isFailed: Boolean = mismatches.nonEmpty
+  }
+
+  type Parser = ParserState => ParserState
+
+  def lit(target: String)(state: ParserState): ParserState = {
+    if (state.isFailed) state
+    else {
+      matchAt(target, state.input, state.index) match {
+        case Some(i) => state.copy(index = i, pending = state.pending :+ Match(i - target.length, i))
+        case _ => state.copy(mismatches = state.mismatches + Mismatch(target, state.index))
+      }
+    }
+  }
+
+  def stringChars(state: ParserState): ParserState = {
+    if (state.isFailed) state
+    else {
+      skipTillQuote(state.input, state.index) match {
+        case Some(i) => state.copy(index = i, pending = state.pending :+ Match(state.index, i))
+        case _ => state.copy(mismatches = state.mismatches + Mismatch("String to be closed", state.index))
+      }
+    }
+  }
+
+  def numChars(state: ParserState): ParserState = {
+    if (state.isFailed) state
+    else {
+      skipNumber(state.input, state.index) match {
+        case Some(i) => state.copy(index = i, pending = state.pending :+ Match(state.index, i))
+        case _ => state.copy(mismatches = state.mismatches + Mismatch("Malformed Number", state.index))
+      }
+    }
+  }
+
+  def sequence(parsers: Parser*)(state: ParserState): ParserState = {
+    require(!parsers.contains(null))
+
+    var i = 0
+    var curState = state
+    while (i < parsers.length && !curState.isFailed) {
+      curState = parsers(i)(curState)
+      i += 1
+    }
+
+    curState
+  }
+
+  def choice(parsers: Parser*)(state: ParserState): ParserState = {
+    require(!parsers.contains(null))
+
+    if (state.isFailed) state
+    else {
+      var i = 0
+      var mismatches = Set.empty[Mismatch]
+      while (i < parsers.length) {
+        val newState = parsers(i)(state)
+        if (!newState.isFailed) return newState
+        mismatches = mismatches ++ newState.mismatches
+        i += 1
+      }
+
+      state.copy(mismatches = mismatches)
+    }
+  }
+
+  def repeat(itemParser: Parser, sepParser: Parser)(state: ParserState): ParserState = {
+    var curState = state
+    while (!curState.isFailed) {
+      curState = itemParser(curState)
+      if (!curState.isFailed) {
+        val state1 = sepParser(curState)
+        if (state1.isFailed) {
+          return curState
+        } else {
+          curState = state1
+        }
+      }
+    }
+    curState
+  }
+
+  def applyJValue(parser: Parser, transformation: Parser): Parser = {
+    parser.andThen(state =>
+      if (state.isFailed) state else {
+        transformation(state).copy(pending = Vector.empty)
+      }
+    )
+  }
+
+  // punctuations
+  val comma: Parser = lit(",")
+  val colon: Parser = lit(":")
+  val quote: Parser = lit("\"")
+  val openObj: Parser = applyJValue(lit("{"), state => state.copy(stack = state.stack :+ _JObjMarker))
+  val closeObj: Parser = lit("}")
+  val openArr: Parser = applyJValue(lit("["), state => state.copy(stack = state.stack :+ _JArrMarker))
+  val closeArr: Parser = lit("]")
+
+  def jValue: Parser = choice(nullValue, trueValue, falseValue, stringValue, numValue, jObj, jArr)
+
+  // terminals
+  val nullValue: Parser = applyJValue(lit("null"), state => {
+    state.copy(
+      stack = state.stack :+ JNull
+    )
+  })
+
+  val trueValue: Parser = applyJValue(lit("true"), state => {
+    state.copy(
+      stack = state.stack :+ JTrue
+    )
+  })
+
+  val falseValue: Parser = applyJValue(lit("false"), state => {
+    state.copy(
+      stack = state.stack :+ JFalse
+    )
+  })
+
+  val stringValue: Parser = applyJValue(sequence(quote, stringChars, quote), state => {
+    val pos = state.pending(state.pending.length - 2)
+    val str = state.input.slice(pos.begin, pos.end)
+    state.copy(
+      stack = state.stack :+ JStr(str)
+    )
+  })
+
+  val numValue: Parser = applyJValue(numChars, state => {
+    val pos = state.pending(state.pending.length - 1)
+    val num = state.input.slice(pos.begin, pos.end).toDouble
+    state.copy(
+      stack = state.stack :+ JNum(num)
+    )
+  })
+
+  // non terminals
+  val jField: Parser = applyJValue(sequence(stringValue, colon, jValue), state => {
+    val pair = _JField(
+      key = state.stack(state.stack.length - 2).asInstanceOf[JStr],
+      value = state.stack(state.stack.length - 1))
+
+    state.copy(stack = state.stack.dropRight(2) :+ pair)
+  })
+
+  private def skipTill(stack: Vector[JValue], marker: JValue): (Vector[JValue], Vector[JValue]) = {
+    val l = stack.length - 1
+    var i = l
+    while (i >= 0 && stack(i) != marker) {
+      i -= 1
+    }
+
+    (stack.take(i), stack.takeRight(l - i))
+  }
+
+  val jObj: Parser = applyJValue(sequence(openObj, repeat(jField, comma), closeObj), state => {
+
+    val (left, right) = skipTill(state.stack, _JObjMarker)
+
+    val fields = right.collect {
+      case _JField(JStr(key), value) => key -> value
+    }.toMap
+
+    state.copy(stack = left :+ JObj(fields))
+  })
+
+  val jArr: Parser = applyJValue(sequence(openArr, repeat(jValue, comma), closeArr), state => {
+    val (left, right) = skipTill(state.stack, _JArrMarker)
+
+    state.copy(stack = left :+ JArr(right))
+  })
+
+  def parse(input: String): JValue = {
+    val state = jValue(ParserState(input))
+    require(!state.isFailed)
+    require(state.index == input.stripTrailing().length)
+    require(state.stack.length == 1)
+
+    state.stack.head
+  }
+}
--- src/main/scala/org/saswata/Main.scala
+++ src/main/scala/org/saswata/Main.scala
@@ -1,25 +1,31 @@
 package org.saswata
 
+import org.saswata.JsonParserCombinator.ParserState
+
 object Main {
+  val input: String =
+    """
+      |{
+      |    "hello": {
+      |        "i am": { "cow" : "boy" },
+      |        "you are": {"cows": [2, {"bessy" : 666}, null]}
+      |    },
+      |    "bad" : false,
+      |    "world": -313.37,
+      |    "foo" : [{"a" : 1}, 2.23, +12.43],
+      |    "bye": "314",
+      |    "good":true,
+      |    "now": null,
+      |    "ok"   :   false
+      |}
+      |""".stripMargin
 
   def main(args: Array[String]): Unit = {
-    val input =
-      """
-        |{
-        |    "hello": {
-        |        "i am": { "cow" : "boy" },
-        |        "you are": {"cows": [2, {"bessy" : 666}, null]}
-        |    },
-        |    "bad" : false,
-        |    "world": -313.37,
-        |    "foo" : [{"a" : 1}, 2.23, +12.43],
-        |    "bye": "314",
-        |    "good":true,
-        |    "now": null
-        |}
-        |""".stripMargin
+    test(new Parser(input).parse())
+    test(JsonParserCombinator.parse(input))
+  }
 
-    val json: Json = new Parser(input).parse()
+  def test(json: JValue): Unit = {
     println(json)
 
     val lookup =
@@ -33,5 +39,44 @@ object Main {
       )
 
     println(lookup)
+  }
+
+  def v2_test(): Unit = {
+    println(JsonParserCombinator.falseValue(ParserState("sadasd")))
+    println(JsonParserCombinator.trueValue(ParserState("true sds ")))
+    println(JsonParserCombinator.nullValue(ParserState("   null dsf   ")))
+    println(JsonParserCombinator.nullValue(ParserState("a   null dsf   ", 3)))
+    println(JsonParserCombinator.openObj(ParserState("a   {null dsf   ", 3)))
+    println(JsonParserCombinator.quote(ParserState("a   \"null dsf   ", 3)))
+    println(JsonParserCombinator.stringValue(ParserState("a   \"null dsf\"   ", 3)))
+
+    println("-------------------")
+
+    println(JsonParserCombinator.jValue(ParserState("   123 ")))
+    println(JsonParserCombinator.jValue(ParserState("   123.456 ")))
+    println(JsonParserCombinator.jValue(ParserState("   +123.456 ")))
+    println(JsonParserCombinator.jValue(ParserState("   -123.456 ")))
+    println(JsonParserCombinator.jValue(ParserState("   1e5 ")))
+    println(JsonParserCombinator.jValue(ParserState("   + 23 ")))
+    println(JsonParserCombinator.jValue(ParserState("   123.12.2 ")))
+    println(JsonParserCombinator.jValue(ParserState("   +23-4 ")))
+
+    println("-------------------")
+
+    println(JsonParserCombinator.jValue(ParserState("   null ")))
+    println(JsonParserCombinator.jValue(ParserState("   true ")))
+    println(JsonParserCombinator.jValue(ParserState("   false ")))
+    println(JsonParserCombinator.jValue(ParserState("   \"whoo\" ")))
+    println(JsonParserCombinator.jValue(ParserState("   \"\" ")))
+    println(JsonParserCombinator.jValue(ParserState("   \"           \" ")))
+    println(JsonParserCombinator.jValue(ParserState("   \"whoo ")))
+    println(JsonParserCombinator.jValue(ParserState("   whoo ")))
+
+    println("-------------------")
+    println(JsonParserCombinator.jField(ParserState("   \"key\" : \"    value    \" ")))
+    println(JsonParserCombinator.jValue(ParserState(" {  \"key\" : \"    value    \"   }  ")))
+    println(JsonParserCombinator.jValue(ParserState(" [   {  \"key\" : \"    value    \"   } ] ")))
+    println(JsonParserCombinator.jValue(ParserState(" [   \"a\" , \"b\"  ,  {  \"key\" :   \"    value    \" , \"k1\" : true  }, null ] ")))
+    println(JsonParserCombinator.jValue(ParserState("[f]")))
   }
 }
